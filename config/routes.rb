eaddd49c9afeacf21e177ca508f3b303ae0931f6
Prelang Builder <builder@prelang.com>
2014-04-09 04:47:00 +0000

    Generated Devise's User model (via Devise's generator)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hub::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
